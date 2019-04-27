cca44996b316fda2c3ec5b033be4786db8f47c43
Lhourquin <lhourquin@gmail.com>
2019-04-27 11:38:40 +0200

    create vue component and root instance of to do list

---
+++ js/main.js
@@ -0,0 +1,26 @@
+Vue.component ('todo-item',{
+  template : '<li>{{title}}<span><button>supprimer</button></span></li>',
+  props : ['title']
+})
+
+new Vue ({
+  el : '#container-to-do-list',
+  data : {
+    newToDo : '',
+    items : [
+    ],
+    nextToDo : 0
+  },
+    methods : {
+    addNewList : function () {
+      this.items.push({
+        id : this.nextToDo++,
+        title : this.newToDo
+      })
+      this.newToDo = ''
+    }
+  }
+})
+
+
+
